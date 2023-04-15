cb229e3d371926b75acfc59d545017b4f05de7d2
RostSuslov <suslovros@gmail.com>
2023-04-15 04:47:35 +0300

    nice example 1

--- script.js
+++ script.js
@@ -1,315 +1,347 @@
-const userArr = [{
-        id: 1,
-        name: "Mark Zuckerberg",
-        age: 38,
-        photo: 'img/photo-MarkZuckerberg.png',
-        gender: 'male',
-    },
-    {
-        id: 2,
-        name: "Claire Boucher",
-        age: 35,
-        photo: 'img/photo-ClaireBoucher.png',
-        gender: 'female',
-    },
-    {
-        id: 3,
-        name: "Ty Simpkins",
-        age: 21,
-        photo: 'img/photo-TySimpkins.png',
-        gender: 'male',
-    },
-    {
-        id: 4,
-        name: "Tom Holland",
-        age: 26,
-        photo: 'img/photo-TomHolland.png',
-        gender: 'male',
-    },
-    {
-        id: 5,
-        name: "Bill Gates",
-        age: 67,
-        photo: 'img/photo-BillGates.png',
-        gender: 'male',
-    },
-    {
-        id: 6,
-        name: "Elon Musk",
-        age: 51,
-        photo: 'img/photo-ElonMusk.png',
-        gender: 'male',
-    },
-    {
-        id: 7,
-        name: "Talulah Jane",
-        age: 37,
-        photo: 'img/photo-TalulahJane.png',
-        gender: 'female',
-    },
-    {
-        id: 8,
-        name: "Bernard Arnault",
-        age: 74,
-        photo: 'img/photo-BernardArnault.png',
-        gender: 'male',
-    },
-    {
-        id: 9,
-        name: "Jeff Bezos",
-        age: 59,
-        photo: 'img/photo-JeffBezos.png',
-        gender: 'male',
-    },
-    {
-        id: 10,
-        name: "Tosca Musk",
-        age: 48,
-        photo: 'img/photo-ToscaMusk.png',
-        gender: 'female',
-    },
-    {
-        id: 11,
-        name: "Steve Jobs",
-        age: 56,
-        photo: 'img/photo-SteveJobs.png', 
-        gender: 'male',
-    },
-    {
-        id: 12,
-        name: "Amber Heard",
-        age: 36,
-        photo: 'img/photo-AmberHeard.png',
-        gender: 'female',
-    },
-    {
-        id: 13,
-        name: "Laurene Powell",
-        age: 59,
-        photo: 'img/photo-LaurenePowell.png',
-        gender: 'female',
-    },
-    {
-        id: 14,
-        name: "Eve Jobs",
-        age: 24,
-        photo: 'img/photo-EveJobs.png',
-        gender: 'female',
-    },
-    {
-        id: 15,
-        name: "Chrisann Brennan",
-        age: 68,
-        photo: 'img/photo-ChrisannBrennan.png', 
-        gender: 'female',
-    },
-    {
-        id: 16,
-        name: "Cara Delevingne",
-        age: 30,
-        photo: 'img/photo-CaraDelevingne.png',
-        gender: 'female',
-    },
-    {
-        id: 17,
-        name: "Cam Gigandet",
-        age: 40,
-        photo: 'img/photo-CamGigandet.png',
-        gender: 'male',
-    },
-    {
-        id: 18,
-        name: "Sean Faris",
-        age: 41,
-        photo: 'img/photo-SeanFaris.png',
-        gender: 'male',
-    },
-    {
-        id: 19,
-        name: "Penn Badgley",
-        age: 41,
-        photo: 'img/photo-PennBadgley.png',
-        gender: 'male',
-    },
-    {
-        id: 20,
-        name: "Camille Vasquez",
-        age: 38,
-        photo: 'img/photo-CamilleVasquez.png',
-        gender: 'female',
-    },
-    {
-        id: 21,
-        name: "Margot Robbie",
-        age: 32, 
-        photo: 'img/photo-MargotRobbie.png',
-        gender: 'female',
-    },
-    {
-        id: 22,
-        name: "Scarlett Johansson",
-        age: 38,
-        photo: 'img/photo-ScarlettJohansson.png',
-        gender: 'female',
-    },
-    {
-        id: 23,
-        name: "Angelina Jolie",
-        age: 47,
-        photo: 'img/photo-AngelinaJolie.png',
-        gender: 'female',
-    },
-    {
-        id: 24,
-        name: "Gal Gadot",
-        age: 37,
-        photo: 'img/photo-GalGadot.png',
-        gender: 'female',
-    },
-    {
-        id: 25,
-        name: "Vito Schnabel",
-        age: 36,
-        photo: 'img/photo-VitoSchnabel.png',
-        gender: 'male',
-    },
-    {
-        id: 26,
-        name: "Dylan Minnette",
-        age: 26,
-        photo: 'img/photo-DylanMinnette.png',
-        gender: 'male',
-    },
-    {
-        id: 27,
-        name: "Ansel Elgort",
-        age: 29,
-        photo: 'img/photo-AnselElgort.png',
-        gender: 'male',
-    },
-    {
-        id: 28,
-        name: "Timothée Chalamet",
-        age: 27,
-        photo: 'img/photo-TimothéeChalame.png',
-        gender: 'male',
-    },
-    {
-        id: 29,
-        name: "Josh Hutcherson",
-        age: 30,
-        photo: 'img/photo-JoshHutcherson.png',
-        gender: 'male',
-    },
-    {
-        id: 30,
-        name: "Aimee Wood",
-        age: 29,
-        photo: 'img/photo-AimeeWood.png',
-        gender: 'female',
-    },
-    {
-        id: 31,
-        name: "Chloë Moretz",
-        age: 26,
-        photo: 'img/photo-ChloëMoretz.png',
-        gender: 'female',
-    },
-    {
-        id: 32,
-        name: "Mimi Keene",
-        age: 24,
-        photo: 'img/photo-MimiKeene.png',
-        gender: 'female',
-    },
-    {
-        id: 33,
-        name: "Elle Fanning",
-        age: 25,
-        photo: 'img/photo-ElleFanning.png',
-        gender: 'female',
-    },
-    {
-        id: 34,
-        name: "Dakota Fanning",
-        age: 29,
-        photo: 'img/photo-DakotaFanning.png',
-        gender: 'female',
-    },
-    {
-        id: 35,
-        name: "Justice Smith",
-        age: 27,
-        photo: 'img/photo-JusticeSmith.png',
-        gender: 'male',
-    },
-    {
-        id: 36,
-        name: "Isabella Sermon",
-        age: 16,
-        photo: 'img/photo-IsabellaSermon.png',
-        gender: 'female',
-    },
-    {
-        id: 37,
-        name: "Omar Sy",
-        age: 45,
-        photo: 'img/photo-OmarSy.png',
-        gender: 'male',
-    },
-]
-let userRenderArr = []
-
-
-const pushArr = (arr1, arr2) => {
-    for (let i = 0; i < arr1.length; i++) {
-        arr2.push(arr1[i]);
-    }
-}
-pushArr(userArr, userRenderArr);
-// console.table(userRenderArr);
-
-
 const ageFilterInput = document.querySelector('#ageFilter');
+const genderFilterInputs = document.querySelectorAll('input[type=radio]');
 const peopleContainer = document.querySelector('.users-container');
-const genderFilterInputs = document.querySelectorAll('input[name="genderFilter"]');
 
-function renderPeople(people) {
-    const peopleHTML = people.map(person => `
-    <div class="user">
-        <div class="person-photo"><img src="${person.photo}" class="img-fluid" alt="${person.id}"></div>
-        <div class="person-name">${person.name}</div>
-        <div class="person-age">${person.age}</div>
-        <div class="person-gender">${person.gender}</div>
-    </div>`).join('');
-    peopleContainer.innerHTML = peopleHTML;
-}
-renderPeople(userRenderArr);
+window.addEventListener('load', function () {
+    const userArr = [{
+            id: 1,
+            name: "Mark Zuckerberg",
+            age: 38,
+            photo: 'img/photo-MarkZuckerberg.png',
+            gender: 'male',
+        },
+        {
+            id: 2,
+            name: "Claire Boucher",
+            age: 35,
+            photo: 'img/photo-ClaireBoucher.png',
+            gender: 'female',
+        },
+        {
+            id: 3,
+            name: "Ty Simpkins",
+            age: 21,
+            photo: 'img/photo-TySimpkins.png',
+            gender: 'male',
+        },
+        {
+            id: 4,
+            name: "Tom Holland",
+            age: 26,
+            photo: 'img/photo-TomHolland.png',
+            gender: 'male',
+        },
+        {
+            id: 5,
+            name: "Bill Gates",
+            age: 67,
+            photo: 'img/photo-BillGates.png',
+            gender: 'male',
+        },
+        {
+            id: 6,
+            name: "Elon Musk",
+            age: 51,
+            photo: 'img/photo-ElonMusk.png',
+            gender: 'male',
+        },
+        {
+            id: 7,
+            name: "Talulah Jane",
+            age: 37,
+            photo: 'img/photo-TalulahJane.png',
+            gender: 'female',
+        },
+        {
+            id: 8,
+            name: "Bernard Arnault",
+            age: 74,
+            photo: 'img/photo-BernardArnault.png',
+            gender: 'male',
+        },
+        {
+            id: 9,
+            name: "Jeff Bezos",
+            age: 59,
+            photo: 'img/photo-JeffBezos.png',
+            gender: 'male',
+        },
+        {
+            id: 10,
+            name: "Tosca Musk",
+            age: 48,
+            photo: 'img/photo-ToscaMusk.png',
+            gender: 'female',
+        },
+        {
+            id: 11,
+            name: "Steve Jobs",
+            age: 56,
+            photo: 'img/photo-SteveJobs.png',
+            gender: 'male',
+        },
+        {
+            id: 12,
+            name: "Amber Heard",
+            age: 36,
+            photo: 'img/photo-AmberHeard.png',
+            gender: 'female',
+        },
+        {
+            id: 13,
+            name: "Laurene Powell",
+            age: 59,
+            photo: 'img/photo-LaurenePowell.png',
+            gender: 'female',
+        },
+        {
+            id: 14,
+            name: "Eve Jobs",
+            age: 24,
+            photo: 'img/photo-EveJobs.png',
+            gender: 'female',
+        },
+        {
+            id: 15,
+            name: "Chrisann Brennan",
+            age: 68,
+            photo: 'img/photo-ChrisannBrennan.png',
+            gender: 'female',
+        },
+        {
+            id: 16,
+            name: "Cara Delevingne",
+            age: 30,
+            photo: 'img/photo-CaraDelevingne.png',
+            gender: 'female',
+        },
+        {
+            id: 17,
+            name: "Cam Gigandet",
+            age: 40,
+            photo: 'img/photo-CamGigandet.png',
+            gender: 'male',
+        },
+        {
+            id: 18,
+            name: "Sean Faris",
+            age: 41,
+            photo: 'img/photo-SeanFaris.png',
+            gender: 'male',
+        },
+        {
+            id: 19,
+            name: "Penn Badgley",
+            age: 41,
+            photo: 'img/photo-PennBadgley.png',
+            gender: 'male',
+        },
+        {
+            id: 20,
+            name: "Camille Vasquez",
+            age: 38,
+            photo: 'img/photo-CamilleVasquez.png',
+            gender: 'female',
+        },
+        {
+            id: 21,
+            name: "Margot Robbie",
+            age: 32,
+            photo: 'img/photo-MargotRobbie.png',
+            gender: 'female',
+        },
+        {
+            id: 22,
+            name: "Scarlett Johansson",
+            age: 38,
+            photo: 'img/photo-ScarlettJohansson.png',
+            gender: 'female',
+        },
+        {
+            id: 23,
+            name: "Angelina Jolie",
+            age: 47,
+            photo: 'img/photo-AngelinaJolie.png',
+            gender: 'female',
+        },
+        {
+            id: 24,
+            name: "Gal Gadot",
+            age: 37,
+            photo: 'img/photo-GalGadot.png',
+            gender: 'female',
+        },
+        {
+            id: 25,
+            name: "Vito Schnabel",
+            age: 36,
+            photo: 'img/photo-VitoSchnabel.png',
+            gender: 'male',
+        },
+        {
+            id: 26,
+            name: "Dylan Minnette",
+            age: 26,
+            photo: 'img/photo-DylanMinnette.png',
+            gender: 'male',
+        },
+        {
+            id: 27,
+            name: "Ansel Elgort",
+            age: 29,
+            photo: 'img/photo-AnselElgort.png',
+            gender: 'male',
+        },
+        {
+            id: 28,
+            name: "Timothée Chalamet",
+            age: 27,
+            photo: 'img/photo-TimothéeChalame.png',
+            gender: 'male',
+        },
+        {
+            id: 29,
+            name: "Josh Hutcherson",
+            age: 30,
+            photo: 'img/photo-JoshHutcherson.png',
+            gender: 'male',
+        },
+        {
+            id: 30,
+            name: "Aimee Wood",
+            age: 29,
+            photo: 'img/photo-AimeeWood.png',
+            gender: 'female',
+        },
+        {
+            id: 31,
+            name: "Chloë Moretz",
+            age: 26,
+            photo: 'img/photo-ChloëMoretz.png',
+            gender: 'female',
+        },
+        {
+            id: 32,
+            name: "Mimi Keene",
+            age: 24,
+            photo: 'img/photo-MimiKeene.png',
+            gender: 'female',
+        },
+        {
+            id: 33,
+            name: "Elle Fanning",
+            age: 25,
+            photo: 'img/photo-ElleFanning.png',
+            gender: 'female',
+        },
+        {
+            id: 34,
+            name: "Dakota Fanning",
+            age: 29,
+            photo: 'img/photo-DakotaFanning.png',
+            gender: 'female',
+        },
+        {
+            id: 35,
+            name: "Justice Smith",
+            age: 27,
+            photo: 'img/photo-JusticeSmith.png',
+            gender: 'male',
+        },
+        {
+            id: 36,
+            name: "Isabella Sermon",
+            age: 16,
+            photo: 'img/photo-IsabellaSermon.png',
+            gender: 'female',
+        },
+        {
+            id: 37,
+            name: "Omar Sy",
+            age: 45,
+            photo: 'img/photo-OmarSy.png',
+            gender: 'male',
+        },
+    ]
 
-ageFilterInput.addEventListener('input', () => {
-    const ageFilterValue = parseInt(ageFilterInput.value);
-    const filteredPeople = userRenderArr.filter(person => person.age <= ageFilterValue);
-    renderPeople(filteredPeople);
-});
+    const renderTemplate = (person) => {
+        return (`
+            <div class="user">
+                <div class="person-photo">
+                <img src="${person.photo}" class="img-fluid" alt="${person.id}"></div>
+                <div class="person-name">${person.name}</div>
+                <div class="person-age">${person.age}</div>
+                <div class="person-gender">${person.gender}</div>
+            </div>
+        `)
+    }
 
-genderFilterInputs.forEach(input => {
-    input.addEventListener('change', () => {
-        if (input.value === "all") {
-            renderPeople(userRenderArr);
-        } else {
-            const genderFilterValue = input.value;
-            const filteredPeople = userRenderArr.filter((person) => person.gender === genderFilterValue);
-            renderPeople(filteredPeople);
+    let list = userArr.map(renderTemplate).join('');
+    peopleContainer.innerHTML = list
+    //корзина
+    let filtered = [];
+    
+    //начальное состояние
+    let initialState = {
+        // age: 100,
+        gender: 'all'
+    }
+
+    ageFilterInput.addEventListener('click', ({target}) => {
+        const age = target.value;  // value инпута по которому кликнул
+
+        initialState.age = parseInt(age);   //начальное состояние меняет age = value
+        console.log(initialState);
+        /*Функция parseInt преобразует первый переданный ей аргумент в строковый тип, интерпретирует его и возвращает целое число или значение NaN . */
+
+        if (initialState.gender === 'all') {
+            filtered = userArr.filter(user => user.age <= initialState.age) // ровняем фильтр по выбранному возрасту
+ 
+            console.table(filtered);
+            let list = filtered.map(renderTemplate).join('');
+
+            peopleContainer.innerHTML = list
+
+            return null
         }
+
+        filtered = userArr.filter(person => {
+            return person.age <= initialState.age && person.gender === initialState.gender
+        })
+
+        let list = filtered.map(renderTemplate).join('');
+
+        peopleContainer.innerHTML = list
+    })
+
+    genderFilterInputs.forEach(input => {
+        input.addEventListener('click', (e) => {
+            const val = e.target.value
+
+            initialState.gender = val;
+
+            if (initialState.gender === 'all') {
+                filtered = userArr.filter(user => {
+                    if (user.age <= initialState.age) {
+                        return user
+                    }
+                })
+
+                let list = filtered.map(renderTemplate).join('');
+                peopleContainer.innerHTML = list
+
+                return null;
+            }
+
+            filtered = userArr.filter(user => {
+                if (user.gender === initialState.gender && (user.age <= initialState.age)) {
+                    return user
+                }
+            })
+
+            let list = filtered.map(renderTemplate).join('');
+            peopleContainer.innerHTML = list
+
+        });
     });
-});
-
-const inputSearch = document.querySelector('#name');
-inputSearch.addEventListener('input', handleInput);
-
-function handleInput() {
-    const filterValue = inputSearch.value;
-    const filteredPersons = userRenderArr.filter(person => person.name.toLowerCase().includes(filterValue.toLowerCase()));
-    console.table(filteredPersons);
-    renderPeople(filteredPersons);
-  }
+})
